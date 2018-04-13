41bb18cc7b3bdb12bfdc884d84210aa95becd508
Victor Fugazzotto <victorfuga@gmail.com>
2018-04-13 08:53:39 -0400

    Updates and refactoring Time helper

--- Assets/Discord-RPC/DiscordTime.cs
+++ Assets/Discord-RPC/DiscordTime.cs
@@ -10,10 +10,7 @@ namespace DiscordPresence
         /// <returns>The current time epoch</returns>
 		public static long TimeNow()
         {
-            TimeSpan time;
-            DateTime today = DateTime.UtcNow;
-            time = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            return (long)time.TotalSeconds;
+            return (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
         }
 
         /// <summary>
@@ -24,12 +21,35 @@ namespace DiscordPresence
         /// <param name="minutes">Minutes to add</param>
         /// <param name="hours">Hours to add</param>
         /// <returns></returns>
+        [Obsolete("AddTime (ref) is deprecated. Use regular add time method instead.")]
         public static long AddTime(ref long time, int seconds, int minutes = 0, int hours = 0)
         {
             time += seconds;
             time += minutes * 60;
             time += hours * 3600;
             return 0;
+        }
+
+        public static long AddTime(long time = -1, int seconds = 0, int minutes = 0, int hours = 0)
+        {
+            if (time == -1)
+                time = TimeNow();
+
+            time += seconds;
+            time += minutes * 60;
+            time += hours * 3600;
+            return time;
+        }
+
+        public static long SubtractTime(long time = -1, int seconds = 0, int minutes = 0, int hours = 0)
+        {
+            if (time == -1)
+                time = TimeNow();
+
+            time -= seconds;
+            time -= minutes * 60;
+            time -= hours * 3600;
+            return time;
         }
 	}
 }
